592e2b7e8f618f84775b136ce60150f063daf6fe
JosipVuckovic <josip.vuckovic@live.com>
2018-05-30 14:40:59 +0200

    promjena zadatak_1

--- PMA_1_Solution/PMA_1/Main.c
+++ PMA_1_Solution/PMA_1/Main.c
@@ -20,7 +20,7 @@ int main()
 }
 void main_menu(void)
 {
-
+	char izbor;
 	printf("******************************************************************\n");
 	printf("*    1 - Zadatak 1 - Izracun potrosnje pretplate za mobitel      *\n");
 	printf("*    2 - Zadatak 2 -                                             *\n");
@@ -30,8 +30,7 @@ void main_menu(void)
 	printf("*    6 - Izlaz                                                    *\n");
 	printf("*******************************************************************\n");
 	printf("         Vas izbor: ");
-	char izbor;
-	scanf_s("%c",&izbor);
+		scanf_s(" %c",&izbor);
 	switch (izbor)
 	{
 	
@@ -55,22 +54,24 @@ void main_menu(void)
 		printf("\nHvala na koristenju\n");
 		printf("pritisnite bilo koju tipku za izlaz\n");
 		getchar();
+		break;
 	default:
 		system("cls");
 		printf("\n\n **Nepoznat odabir, molim Vas ponovite unos**\n\n");
 		main_menu();
+		break;
 
 	} 
 	
 	}
 void do_you_want_more(void)
 {
+	char odabir;
 	printf("****************************************************************\n");
 	printf("*Zelite li drugi zadatak?*\n");
 	printf("****************************************************************\n");
 	printf("y/n:\n");
-	char odabir;
-	scanf_s("%c", &odabir);
+	scanf_s(" %c", &odabir);
 	switch (odabir)
 	{
 	
@@ -95,12 +96,27 @@ void do_you_want_more(void)
 	default:
 		printf("\nNepoznat unos!\n");
 		do_you_want_more();
+		break;
 		
 		
 	}
 }
 	void zadatak_1(void)
 	{
+		
+		float pdv;
+		float iznos_pretplate;
+		float minuta_preplata;
+		float sms_pretplata;
+		float cijena_poziva;
+		float cijena_sms;
+		float potroseno_sati_razgovora = 0;
+		float potroseno_minuta_razgovora = 0;
+		float poslanih_sms = 0;
+		float potroseno_razgovora_ukupno;
+		float za_doplatu_razgovora;
+		float za_doplatu_sms;
+		float za_platiti_ukupno;
 		system("cls");
 		printf("**********************************************************************\n");
 		printf("*							Zadatak 1                                *\n");
@@ -113,22 +129,22 @@ void do_you_want_more(void)
 		printf("Podaci o pretplati: \n");
 		printf("**********************************************************************\n");
 		printf("PDV: ");
-			float pdv;
+			
 			scanf_s("%f", &pdv);
 		printf("\nIznos pretplate bez PDV-a: ");
-			float iznos_pretplate;
+			
 			scanf_s("%f", &iznos_pretplate);
 		printf("\nUnesite kolicinu minuta u pretplati: ");
-			float minuta_preplata;
+			
 			scanf_s("%f", &minuta_preplata);
 		printf("\nUnesite kolicina poruka u pretplati: ");
-			float sms_pretplata;
+			
 			scanf_s("%f", &sms_pretplata);
 		printf("\nUnesite cijenu poziva po minuti, bez PDV-a: ");
-			float cijena_poziva;
+			
 			scanf_s("%f", &cijena_poziva);
 		printf("\nUnesite cijenu sms-a: ");
-			float cijena_sms;
+			
 			scanf_s("%f", &cijena_sms);
 		printf("\n");
 		printf("**********************************************************************\n");
@@ -136,20 +152,18 @@ void do_you_want_more(void)
 		printf("Podaci o potrosnji: \n");
 		printf("**********************************************************************\n");
 		printf("\nSati razgovora: ");
-			float potroseno_sati_razgovora=0;
+			
 			scanf_s("%f", &potroseno_sati_razgovora);
 		printf("\nMinuta razgovora: ");
-			float potroseno_minuta_razgovora=0;
+			
 			scanf_s("%f", &potroseno_minuta_razgovora);
 		printf("\nPoslano sms-ova ");
-			float poslanih_sms=0;
+			
 			scanf_s("%f", &poslanih_sms);
 		printf("**********************************************************************\n");
-		float potroseno_razgovora_ukupno;
+		
 		potroseno_razgovora_ukupno = potroseno_minuta_razgovora + (potroseno_sati_razgovora * 60);
-		float za_doplatu_razgovora;
-		float za_doplatu_sms;
-		float za_platiti_ukupno;
+		
 		switch (minuta_preplata>=potroseno_razgovora_ukupno)
 		{
 		case 0:
